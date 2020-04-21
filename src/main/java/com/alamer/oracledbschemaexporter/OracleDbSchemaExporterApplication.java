9dfe0e2945eb9fdf52f96473f138d9cf0c5fba88
Щербаков Иван Вячеславович_4 <ivan.v.shcherbakov@domru.ru>
2020-04-21 16:39:29 +0800

    initial commit

---
+++ src/main/java/com/alamer/oracledbschemaexporter/OracleDbSchemaExporterApplication.java
@@ -0,0 +1,13 @@
+package com.alamer.oracledbschemaexporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleDbSchemaExporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OracleDbSchemaExporterApplication.class, args);
+    }
+
+}
